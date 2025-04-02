3de86a1b608513ceb1c3991fb65f4eecaf7fb73f
E.Moraes <emmanoelnascimento37@gmail.com>
2025-04-02 13:10:31 -0300

    Exibe mensagem de texto com múltiplos comandos

---
+++ chapter2/examples/Welcome2.java
@@ -0,0 +1,17 @@
+package chapter2.examples; // pertece ao package
+// Figura 2.3: Welcome.java
+// Imprime uma linha de texto com múltiplas instruções.
+
+
+
+public class Welcome2 {
+
+    // método main inicia execução do  aplicativo Java
+    public static void main (String[] args){
+
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+
+    } // fim do método main
+
+} // fim da classe Welcome2
