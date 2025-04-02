1137b5d7e8b02cb77d80cd23ecf49fb4551c00c8
E.Moraes <emmanoelnascimento37@gmail.com>
2025-04-02 12:54:33 -0300

    First book's program

---
+++ chapter2/examples/Welcome1.java
@@ -0,0 +1,16 @@
+package chapter2.examples; //pertence ao package
+
+// Figura 2.1: Welcome.java
+// Programa de impressão de texto.
+
+
+public class Welcome1 {
+
+    // método main incia execução do aplicativo Java
+    public static void main (String[] args){
+
+        System.out.println("Welcome to Java Programming");
+
+    } // fim da main
+
+} // fim da  classe Welcome1
