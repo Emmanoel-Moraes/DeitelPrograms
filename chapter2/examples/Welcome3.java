22c3acacd409d4fc3cc3cdddbc65c99d962b2272
E.Moraes <emmanoelnascimento37@gmail.com>
2025-04-10 10:28:10 -0300

    Exemplo 4

---
+++ chapter2/examples/Welcome3.java
@@ -0,0 +1,15 @@
+package chapter2.examples; // pertence ao package
+// Figura 2.4: Welcome3.java
+// Imprimindo múltiplas linhas com apenas uma única instrução
+
+
+public class Welcome3 {
+
+    // método main inicia execução do  aplicativo Java
+    public static void main (String[] args){
+
+        System.out.println("Welcome\nto\nJava\nProgramming!");
+
+    } // fim do método main
+
+} // fim da classe Welcome3
---
+++ chapter2/examples/Welcome4.java
@@ -0,0 +1,15 @@
+package chapter2.examples; // pertecne ao package
+// Exibindo  múltiplas linhas com método System.out.printf.
+
+
+
+public class Welcome4 {
+
+    // método main inicia a execução do aplicativo Java
+    public static void main (String [] args){
+
+        System.out.printf("%s%n%s%n", "Welcome to", "Java Programming");
+
+    } // fim  da  main
+
+} // fim da classe Welcome4
